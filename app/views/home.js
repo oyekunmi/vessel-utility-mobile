9a0fcb12f39df04dd7abd1bf54b612ed24d20914
Oyesola Ogundele <oyekunmi@gmail.com>
2019-03-28 16:33:35 +0100

    firebase: introduced firebase to login page to handle authentication

--- app/views/home.js
+++ app/views/home.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { View, Text } from "react-native";
+import constants from '../constants';
 
+// const 
+const styles = ({
+  background: {
+    backgroundColor: constants.APP_BACKGROUND_COLOR,
+    flex: 1,
+  }
+});
 const HomeScreen  = (props) => {
   const {navigate} = props.navigation;
 
   return (
-    <View>
+    <View style={styles.background}>
       <Text>Home</Text>
     </View>
   );
 };
+
+
  
 export default HomeScreen;
